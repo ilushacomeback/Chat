5b3f9206809c177ce8ff626758ad95b8c6f7571d
ilushacomeback <vip.ilya.rud.2016@mail.com>
2024-05-09 11:18:30 +0300

    test

--- frontend/src/components/Chat/Messages.jsx
+++ frontend/src/components/Chat/Messages.jsx
@@ -20,7 +20,7 @@ const Messages = () => {
   const renderMessage = (message) => (
     <div className="text-break mb-2" key={message.id}>
       <b>{message.username}</b>
-      :
+      {': '}
       {message.body}
     </div>
   );
